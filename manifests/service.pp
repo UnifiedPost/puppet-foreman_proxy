bbc133c5c6afa816596352bbbda8af55fd06ca24
Pieter-Jan Gunst <pjgunst@inuits.eu>
2012-07-05 10:24:34 +0200

    #DEVOPS-158 - fix foreman_proxy not being enabled by default

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,6 +2,9 @@ class foreman_proxy::service {
 
   service { 'foreman-proxy':
     ensure     => running,
+    enable     => true,
+    hasstatus  => true,
+    hasrestart => true,
     require    => Class['foreman_proxy::config'],
   }
 
